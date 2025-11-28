845b580cc3d720e392055d45f3ac0f25428be9ac
vanda-alex <vanda.alekseeva@student.unisg.ch>
2025-11-28 10:22:28 +0100

    test

--- project CS/app.py
+++ project CS/app.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
+
 st.write("Hello World")
+#creating columns to easily orient objects on the page
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+ st.write("Welcome to:")
+ st.title("*COOKABLE*")
+ st.header("Because Googling 'chicken recipe' for the 47th time is exhausting.")
+
+
+#inserting the image of a dancing chef lottie
+#preparing the ground to import the lottie with url
+from streamlit_lottie import st_lottie
+import requests
+def load_lottie_url(url):
+   r = requests.get(url)
+   if r.status_code != 200:
+       return None
+   return r.json()
+#actual lottie
+lottie_cooking = load_lottie_url("https://lottiefiles.com/free-animation/3d-chef-dancing-NQIgrnDaSK")
+st_lottie(lottie_cooking, height=300, key="cooking")
+
+
