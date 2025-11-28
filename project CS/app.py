2cf4dc8f2f9699480e689bfdb0f5b81c5ed0d0f7
vanda-alex <vanda.alekseeva@student.unisg.ch>
2025-11-28 10:58:05 +0100

    test

--- project CS/app.py
+++ project CS/app.py
@@ -7,20 +7,9 @@ col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
  st.write("Welcome to:")
  st.title("*COOKABLE*")
- st.header("Because Googling 'chicken recipe' for the 47th time is exhausting.")
-
-
-#inserting the image of a dancing chef lottie
-#preparing the ground to import the lottie with url
-from streamlit_lottie import st_lottie
-import requests
-def load_lottie_url(url):
-   r = requests.get(url)
-   if r.status_code != 200:
-       return None
-   return r.json()
-#actual lottie
-lottie_cooking = load_lottie_url("https://lottiefiles.com/free-animation/3d-chef-dancing-NQIgrnDaSK")
-st_lottie(lottie_cooking, height=300, key="cooking")
-
+# centering the slogan in the middle of the page using HTML
+    st.markdown(
+        "<h3 style='text-align:center'>Because Googling 'chicken recipe' for the 47th time is exhausting.</h3>",
+        unsafe_allow_html=True,
+    )
 
