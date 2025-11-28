c011e71a51f1866fd251d1dd080331dad816a75b
vanda-alex <vanda.alekseeva@student.unisg.ch>
2025-11-28 12:20:49 +0100

    ok

--- project CS/app.py
+++ project CS/app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+#creating columns to center the welcome statement
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.write("Welcome to")
+# using HTML and CSS to make the title big and centered 
 st.markdown(
     """
     <style>
@@ -7,14 +12,11 @@ st.markdown(
       .big-sub   { font-size: clamp(18px, 3vw, 28px); color: #555; text-align: center; margin: 6px 0 18px 0; }
     </style>
     <div style="width: 100%;">
-      <div class="big-title">*COOKABLE*</div>
-      <div class="big-sub">Because Googling 'chicken recipe' for the 47th time is exhausting.</div>
+      <div class="big-title">COOKABLE</div>
     </div>
     """,
     unsafe_allow_html=True,
 )
 
-# Optional: keep small column section for other content
-col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.write("Welcome to")
+    st.write("Because googling 'chicken recipe' for the 47th time is exhausting. ")
